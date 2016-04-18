c04a97a49e438fc7cf1c70f174ae6da3d7bde8ff
AtillaSzikora <szikora.atilla@gmail.com>
2016-04-18 16:27:48 +0200

    Threads added

--- SimpleThreadingDemo/SimpleThreadingDemo.cs
+++ SimpleThreadingDemo/SimpleThreadingDemo.cs
@@ -1,13 +1,22 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 
 namespace SimpleThreadingDemo {
 	internal class SimpleThreadingDemo {
 
-		private static void Counting () {
+		static void Main (string[] args) {
+			ThreadStart starter = new ThreadStart(Counting);
+			Thread first = new Thread(starter);
+			Thread second = new Thread(starter);
+			first.Start();
+			second.Start();
+			first.Join();
+			second.Join();
+			Console.Read(); }
 
-			for (int i = 0; i < 10; i++) {
-
-			}
-		}
+		static void Counting() {
+			for (int i = 1; i <= 10; i++) {
+				Console.WriteLine("Count: {0} - Thread: {1}", i, Thread.CurrentThread.ManagedThreadId);
+				Thread.Sleep(10); } }
 	}
 }
