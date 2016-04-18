8050306c7786c477dd99b17ce4c0ea00811e0f42
AtillaSzikora <szikora.atilla@gmail.com>
2016-04-18 10:41:59 +0200

    Class structure added.

---
+++ SimpleThreadingDemo/SimpleThreadingDemo.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+
+namespace SimpleThreadingDemo {
+	internal class SimpleThreadingDemo {
+
+		private static void Counting () {
+
+			for (int i = 0; i < 10; i++) {
+
+			}
+		}
+	}
+}
