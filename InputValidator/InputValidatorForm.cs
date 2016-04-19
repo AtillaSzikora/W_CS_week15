0789bdea18e131b90f2b460a6d840c6326cc604a
AtillaSzikora <szikora.atilla@gmail.com>
2016-04-19 15:29:51 +0200

    InputValidatorForm added

---
+++ InputValidator/InputValidatorForm.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Forms;
+
+namespace InputValidator {
+	public partial class InputValidatorForm : Form {
+		public InputValidatorForm () {
+			InitializeComponent ();
+		}
+	}
+}
