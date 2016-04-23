544405037f768ef9e64daf376ad42c0432b70fde
AtillaSzikora <szikora.atilla@gmail.com>
2016-04-23 14:03:41 +0200

    Regex added to wform and works

--- InputValidator/InputValidatorForm.Designer.cs
+++ InputValidator/InputValidatorForm.Designer.cs
@@ -61,6 +61,7 @@
 			this.btnSave.TabIndex = 3;
 			this.btnSave.Text = "Save";
 			this.btnSave.UseVisualStyleBackColor = false;
+			this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
 			// 
 			// lblName
 			// 
