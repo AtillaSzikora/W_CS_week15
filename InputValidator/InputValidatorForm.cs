b2539a8e911dbb7448c8f600ce187f66b04685aa
AtillaSzikora <szikora.atilla@gmail.com>
2016-04-23 14:21:59 +0200

    ReformatPhone field added

--- InputValidator/InputValidatorForm.cs
+++ InputValidator/InputValidatorForm.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 using System.Text.RegularExpressions;
 
 namespace InputValidator {
@@ -9,9 +10,16 @@ namespace InputValidator {
 		private void btnSave_Click (object sender, System.EventArgs e) {
 			if (!Regex.IsMatch(txtName.Text, @"^([A-Za-z]*\s*)*$")) 
 				MessageBox.Show(@"The name is invalid (only alphabetical characters are allowed)"); 
-			if (!Regex.IsMatch(txtPhone.Text, @"^((\(\d{3}\)?)|(\d{3}-))?\d{3}-\d{4}$")) 
+			if (!Regex.IsMatch(txtPhone.Text, @"^\(?(\d{3})\)?[\s\-]?(\d{3})\-?(\d{4})$")) 
 				MessageBox.Show(@"The phone number is not a valid US phone number"); 
-			if (!Regex.IsMatch(txtEmail.Text, @"^([a-zA-Z0-9_\-” + @”\.]+)@((\[[0-9]{1,3}" + @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" + @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$")) 
-				MessageBox.Show(@"The e-mail address is not valid."); }
+			if (!Regex.IsMatch(txtEmail.Text, @"^([a-zA-Z0-9_\-” + @”\.]+)@((\[[0-9]{1,3}" +
+				@"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
+				@".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$")) 
+				MessageBox.Show(@"The e-mail address is not valid.");
+			txtPhone.Text = ReformatPhone(txtPhone.Text); }
+
+		static string ReformatPhone(string s) { 
+			Match m = Regex.Match(s, @"^\(?(\d{3})\)?[\s\-]?(\d{3})\-?(\d{4})$");
+			return $"({m.Groups[1]}) {m.Groups[2]}-{m.Groups[3]}"; }
 	}
 }
